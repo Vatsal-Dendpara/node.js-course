5e6d2bb6c6d8043e92554106d0a835e5a2e40a51
Vatsal <vatsal.dendpara@marutitech.com>
2022-06-27 15:45:05 +0530

    Assignment 1 completed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const request = require("postman-request");
+const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const csvwriter = require("csv-writer");
 const getRepoData = require("./utils/getRepoData");
+
+const port = process.env.PORT || 3000;
 
 //setup handlebars engine and views locations
 app.set("view engine", "hbs");
@@ -18,15 +22,19 @@ app.get("/", (req, res) => {
   });
 });
 
+//get data according to textboxes
 app.get("/repo", (req, res) => {
-  getRepoData(req.query.lang, req.query.forks, (error, data) => {
-    if (error) {
-      return res.send({
-        error,
-      });
-    }
-    res.send({
-      data: data.map((chunk) => {
+  getRepoData(
+    req.query.lang,
+    req.query.forks,
+    req.query.stargazers_count,
+    (error, data) => {
+      if (error) {
+        return res.send({
+          error,
+        });
+      }
+      const fileData = data.map((chunk) => {
         return {
           name: chunk.name,
           description: chunk.description,
@@ -37,10 +45,34 @@ app.get("/repo", (req, res) => {
           forks_count: chunk.forks_count,
           language: chunk.language,
         };
-      }),
-    });
-  });
+      });
+
+      res.send({
+        data: fileData,
+      });
+      // CSV conversion using csv-parser module
+      const createCSVWriter = csvwriter.createObjectCsvWriter;
+      const csvWriter = createCSVWriter({
+        path: "./data.csv",
+        header: [
+          { id: "name", title: "NAME" },
+          { id: "description", title: "DESCRIPTION" },
+          { id: "html_url", title: "HTML_URL" },
+          { id: "forks", title: "FORKS" },
+          { id: "watchers_count", title: "WATCHERS_COUNT" },
+          { id: "stargazers_count", title: "STARGAZERS_COUNT" },
+          { id: "forks_count", title: "FORKS_COUNT" },
+          { id: "language", title: "LANGUAGE" },
+        ],
+      });
+
+      csvWriter.writeRecords(fileData).then(() => {
+        console.log("done");
+      });
+    }
+  );
 });
-app.listen(3000, () => {
+
+app.listen(port, () => {
   console.log("server is running");
 });
--- src/utils/getRepoData.js
+++ src/utils/getRepoData.js
@@ -1,11 +1,18 @@
 const request = require("postman-request");
 let url = "";
-const getRepoData = (lang, forks = 0, callback) => {
+const getRepoData = (lang, forks = 0, stargazers_count = 2000, callback) => {
   if (lang == null) {
     url = "https://api.github.com/search/repositories?q=is:public";
-  } else {
+  } else if (forks == null) {
     url =
       "https://api.github.com/search/repositories?q=is:public+language:" + lang;
+  } else {
+    url =
+      "https://api.github.com/search/repositories?q=is:public+language:" +
+      lang +
+      "+" +
+      "forks:>=" +
+      forks;
   }
   request(
     {
@@ -22,7 +29,9 @@ const getRepoData = (lang, forks = 0, callback) => {
         callback("unable to find data", undefined);
       } else {
         const repos = body.items.filter((repo) => {
-          return repo.forks >= forks && repo.stargazers_count > 2000;
+          return (
+            repo.forks >= forks && repo.stargazers_count > stargazers_count
+          );
         });
 
         callback(undefined, repos);
