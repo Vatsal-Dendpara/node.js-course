e87f1f6e3cb020037fcea34d78a0ff3a5a7b5abc
Vatsal <vatsal.dendpara@marutitech.com>
2022-08-01 15:21:39 +0530

    tested: nest-demo app

--- nest-demo/src/users/users.controller.spec.ts
+++ nest-demo/src/users/users.controller.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockUsersService = {
+    createUser: jest.fn((dto) => {
+      return {
+        id: Date.now(),
+        ...dto,
+      };
+    }),
+    findAll: jest.fn(() => {
+      return {
+        id: 1,
+        name: 'marius',
+      };
+    }),
+    findById: jest.fn((id) => {
+      return {
+        id,
+        name: 'marius',
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [UsersService],
+    })
+      .overrideProvider(UsersService)
+      .useValue(mockUsersService)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a user', () => {
+    expect(controller.createUser({ name: 'Vatsal' })).toEqual({
+      id: expect.any(Number),
+      name: 'Vatsal',
+    });
+  });
+
+  it('should get user', async () => {
+    expect(await controller.getUsers('marius')).toEqual({
+      id: 1,
+      name: 'marius',
+    });
+  });
+
+  it('should get user by id', async () => {
+    expect(await controller.getUserById(1)).toEqual({
+      id: 1,
+      name: 'marius',
+    });
   });
 });
--- nest-demo/test/app.e2e-spec.ts
+++ nest-demo/test/app.e2e-spec.ts
@@ -1,24 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+//import { AppModule } from './../src/app.module';
+import { UsersModule } from '../src/users/users.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [UsersModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('should get all users (GET)', () => {
+    return request(app.getHttpServer()).get('/users').expect(200);
+  });
+
+  it('should get user by its name (GET)', () => {
+    return request(app.getHttpServer()).get('/users?name=John').expect(200);
+  });
+
+  it('should get user by id (GET)', async () => {
+    return request(app.getHttpServer()).get('/users/1').expect(200);
+  });
+
+  it('should create a new user (POST)', async () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/users')
+      .send({ id: 5, name: 'test user' })
+      .expect(201);
   });
 });
