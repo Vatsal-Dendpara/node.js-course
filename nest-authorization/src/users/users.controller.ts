2f7664cd62db6e2be6ffea72cee2386c4db93351
Vatsal Dendpara <vatsal.dendpara@marutitech.com>
2022-08-10 17:30:25 +0530

    file upload tutorial done in nest

--- nest-authorization/src/users/users.controller.ts
+++ nest-authorization/src/users/users.controller.ts
@@ -6,20 +6,38 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
+  FileTypeValidator,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Roles } from './roles.decorator';
 import { Role } from './entities/role.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @Roles(Role.ADMIN)
-  create(@Body() createUserDto: CreateUserDto) {
+  @UseInterceptors(FileInterceptor('file'))
+  //@Roles(Role.ADMIN)
+  create(
+    @Body() createUserDto: CreateUserDto,
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new FileTypeValidator({ fileType: 'jpeg' })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+
     return this.usersService.create(createUserDto);
   }
 
